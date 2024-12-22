a4d627e59756b9477c57e0cbff7067e707da676a
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2024-12-23 01:58:20 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "68e657b10240cb7db1e0360152bf1b53";
+const API_KEY = "Your API Key";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
